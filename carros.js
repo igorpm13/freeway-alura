1b03e83a2bcd9916421c7b3c9e3a2d8dfd74ac8b
Igor Moreira <igorpel13@gmail.com>
2023-02-27 10:58:24 -0300

    implementação For em MovimentoCarros/ resetPosCarros

--- carros.js
+++ carros.js
@@ -7,28 +7,19 @@ let velCarros = [ 2, 3, 2.5];
 let inicioCarros = 600;
 
 function invocCarros(){
-  for(let i = 0; i < imagemCarros.length; i = i+1){
+  for(let i = 0; i < imagemCarros.length; i += +1){
     image(imagemCarros[i] , xCarros[i] , yCarros[i] , altCarros , largCarros);
   }
 }
 function movimentoCarros(){
-  xCarros[0] -= velCarros[0];
-
-  xCarros[1] -= velCarros[1];
-  
-  xCarros[2] -= velCarros[2];
-  
+    for(let j = 0; j < xCarros.length; j += +1 ){
+    xCarros[j] -= velCarros[j]; 
+  }
 }
 function resetPosCarros(){
-  if( xCarros[0] < -50 ){
-    xCarros[0] = inicioCarros ;
-  }
-  if( xCarros[1] < -50 ){
-    xCarros[1] = inicioCarros ;
-  }
-  if( xCarros[2] < -50 ){
-    xCarros[2] = inicioCarros ;
-  }
-
+  for (let k = 0; k < xCarros.length; k += +1 )
+    if( xCarros[k] < -50 ){
+      xCarros[k] = inicioCarros ;
+    }
 }
 
